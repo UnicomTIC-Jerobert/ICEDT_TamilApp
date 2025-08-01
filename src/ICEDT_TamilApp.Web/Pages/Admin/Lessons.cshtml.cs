31e8a51e293cbd9f1eb9f3598c3ebe00b564d030
jerun276 <jerunkingston@gmail.com>
2025-08-01 12:59:43 +0530

    Revert "Merge branch 'main' of https://github.com/UnicomTIC-Jerobert/ICEDT_TamilApp"
    This reverts commit 5f786c6cbc32d99026a3ac3549cec5718d4dfef5, reversing
    changes made to 095fd3d19b0cb1bc1e9a1940169ec9489daa4ad9.

---
+++ src/ICEDT_TamilApp.Web/Pages/Admin/Lessons.cshtml.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ICEDT_TamilApp.Web.Pages.Admin
+{
+    public class LessonsModel : PageModel
+    {
+        public void OnGet() { }
+    }
+}
---
+++ src/ICEDT_TamilApp.Web/Pages/Admin/Levels.cshtml.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ICEDT_TamilApp.Web.Pages.Admin
+{
+    // You can add [Authorize] attribute here later to protect the page
+    public class LevelsModel : PageModel
+    {
+        public void OnGet() { }
+    }
+}
